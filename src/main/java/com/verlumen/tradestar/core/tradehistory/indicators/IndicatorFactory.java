a278c662af7fbd1b1414271546773a86238bca65
Patrick Selamy <pselamy@gmail.com>
2022-04-30 22:05:09 -0500

    Update IndicatorFactory.

--- src/main/java/com/verlumen/tradestar/core/tradehistory/indicators/IndicatorFactory.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/indicators/IndicatorFactory.java
@@ -3,8 +3,8 @@ package com.verlumen.tradestar.core.tradehistory.indicators;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.verlumen.tradestar.protos.indicators.Indicator;
-import com.verlumen.tradestar.protos.indicators.IndicatorParams;
 import org.ta4j.core.BarSeries;
+import org.ta4j.core.num.Num;
 
 import java.util.Optional;
 import java.util.Set;
@@ -13,7 +13,7 @@ import static com.google.common.base.Functions.identity;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 
 public class IndicatorFactory {
-    private final ImmutableMap<IndicatorParams.TypeCase, TypedIndicatorFactory>
+    private final ImmutableMap<Indicator.Params.TypeCase, TypedIndicatorFactory>
             indicatorFactories;
 
     @Inject
@@ -24,15 +24,26 @@ public class IndicatorFactory {
                         TypedIndicatorFactory::supportedParams, identity()));
     }
 
-    public Indicator create(BarSeries barSeries, IndicatorParams params) {
+    public TypedIndicator create(BarSeries barSeries, Indicator.Params params) {
         return Optional.ofNullable(indicatorFactories.get(params.getTypeCase()))
                 .map(factory -> factory.create(barSeries, params))
                 .orElseThrow(UnsupportedOperationException::new);
     }
 
     public interface TypedIndicatorFactory {
-        Indicator create(BarSeries barSeries, IndicatorParams params);
+        TypedIndicator create(BarSeries barSeries, Indicator.Params params);
 
-        IndicatorParams.TypeCase supportedParams();
+        Indicator.Params.TypeCase supportedParams();
+    }
+
+    public interface TypedIndicator {
+        org.ta4j.core.Indicator<Num> driver();
+
+        Indicator indicator(int index);
+
+        default double value(int index) {
+            return driver().getValue(index).doubleValue();
+        }
+
     }
 }
