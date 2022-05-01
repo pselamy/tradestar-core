4c6e0ba42a68a368304de6b66ef2002627416a83
Patrick Selamy <pselamy@gmail.com>
2022-05-01 11:53:08 -0500

    Add `indicator_adapter_repository_impl`.

---
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/indicators/IndicatorAdapterRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.verlumen.tradestar.core.tradehistory.indicators;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+import com.verlumen.tradestar.protos.indicators.Indicator;
+import org.ta4j.core.BarSeries;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static com.google.common.base.Functions.identity;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
+public class IndicatorAdapterRepositoryImpl implements IndicatorAdapterRepository {
+    private final ImmutableMap<Indicator.Params.TypeCase, IndicatorAdapterFactory>
+            adapterFactories;
+
+    @Inject
+    IndicatorAdapterRepositoryImpl(Set<IndicatorAdapterFactory> adapterFactories) {
+        this.adapterFactories = adapterFactories
+                .stream()
+                .collect(toImmutableMap(
+                        IndicatorAdapterFactory::typeCase, identity()));
+    }
+
+    @Override
+    public IndicatorAdapter get(Indicator.Params params, BarSeries barSeries) {
+        return Optional.ofNullable(adapterFactories.get(params.getTypeCase()))
+                .map(factory -> factory.create(barSeries, params))
+                .orElseThrow(UnsupportedOperationException::new);
+    }
+
+}
