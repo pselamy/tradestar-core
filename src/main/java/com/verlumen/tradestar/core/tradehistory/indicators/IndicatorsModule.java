c2dee1961f36ba61f4c71041c6a84701ee4bd3cb
Patrick Selamy <pselamy@gmail.com>
2022-04-30 23:09:20 -0500

    Update IndicatorsModule.

--- src/main/java/com/verlumen/tradestar/core/tradehistory/indicators/IndicatorsModule.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/indicators/IndicatorsModule.java
@@ -1,16 +1,20 @@
 package com.verlumen.tradestar.core.tradehistory.indicators;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import com.verlumen.tradestar.core.tradehistory.indicators.IndicatorFactory.IndicatorAdapterFactory;
 
 public class IndicatorsModule extends AbstractModule {
+    private static final ImmutableSet<Class<? extends IndicatorAdapterFactory>>
+            ADAPTERS = ImmutableSet.of(AdxIndicatorAdapterFactory.class);
+
     @Override
     protected void configure() {
         Multibinder<IndicatorAdapterFactory> indicatorAdapterFactoryBinder =
                 Multibinder.newSetBinder(binder(),
                         IndicatorAdapterFactory.class);
-        indicatorAdapterFactoryBinder.addBinding().to(
-                AdxIndicatorAdapterFactory.class);
+        ADAPTERS.forEach(adapter ->indicatorAdapterFactoryBinder.addBinding()
+                .to(adapter));
     }
 }
