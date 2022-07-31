6c77a7d678361b98cc514bdec87c44ee65761e8f
Patrick Selamy <pselamy@gmail.com>
2022-07-30 21:40:35 -0500

    Add merging (#9)
    * Update CandleFactory.
    
    * Use Java 11.
    
    * Add candle merging.

--- src/main/java/com/verlumen/tradestar/core/candles/CandleFactory.java
+++ src/main/java/com/verlumen/tradestar/core/candles/CandleFactory.java
@@ -1,34 +1,64 @@
 package com.verlumen.tradestar.core.candles;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.time.Instant.ofEpochSecond;
-
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.math.Stats;
 import com.verlumen.tradestar.protos.candles.Candle;
 import com.verlumen.tradestar.protos.candles.Granularity;
 import com.verlumen.tradestar.protos.trading.ExchangeTrade;
+
 import java.time.Instant;
 import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static com.google.common.collect.MoreCollectors.onlyElement;
+import static java.time.Instant.ofEpochSecond;
+import static java.util.Comparator.comparing;
 
 public class CandleFactory {
   public static Candle create(CreateParams params) {
     checkArgument(!params.trades().isEmpty());
     checkArgument(GranularitySpec.isSupported(params.granularity()));
 
-    ExchangeTrade firstTrade = params.trades().get(0);
-    ExchangeTrade lastTrade = params.trades().get(params.trades().size() - 1);
+    ImmutableList<ExchangeTrade> trades = sortTrades(params.trades());
+    ExchangeTrade firstTrade = trades.get(0);
+    ExchangeTrade lastTrade = trades.get(trades.size() - 1);
     GranularitySpec granularitySpec = GranularitySpec.fromGranularity(params.granularity());
     Instant startTime = getStartTime(granularitySpec, firstTrade);
     Instant endTime = startTime.plus(granularitySpec.duration());
     Instant lastTradeTime = ofEpochSecond(lastTrade.getTimestamp().getSeconds());
     checkArgument(lastTradeTime.isBefore(endTime));
-    return create(startTime, params.trades());
+    return createCandle(startTime, trades);
+  }
+
+  public static Candle mergeCandles(MergeParams params) {
+    ImmutableList<Candle> candles = params.candles().asList();
+    Candle firstCandle = candles.get(0);
+    Candle lastCandle = candles.get(candles.size() - 1);
+
+    double volume = params.candles().stream().mapToDouble(Candle::getVolume).sum();
+    return Candle.newBuilder()
+        .setStart(firstCandle.getStart())
+        .setOpen(firstCandle.getOpen())
+        .setHigh(
+            candles.stream().mapToDouble(Candle::getHigh).max().stream()
+                .boxed()
+                .collect(onlyElement()))
+        .setLow(
+            candles.stream().mapToDouble(Candle::getLow).min().stream()
+                .boxed()
+                .collect(onlyElement()))
+        .setClose(lastCandle.getClose())
+        .setVolume(volume)
+        .build();
   }
 
   @SuppressWarnings("UnstableApiUsage")
-  private static Candle create(Instant startTime, ImmutableList<ExchangeTrade> trades) {
+  private static Candle createCandle(Instant startTime, ImmutableList<ExchangeTrade> trades) {
     ExchangeTrade firstTrade = trades.get(0);
     ExchangeTrade lastTrade = trades.get(trades.size() - 1);
     DoubleStream prices =
@@ -48,6 +78,13 @@ public class CandleFactory {
     return builder.build();
   }
 
+  private static ImmutableList<ExchangeTrade> sortTrades(ImmutableSet<ExchangeTrade> trades) {
+    return trades.stream()
+        .sorted(comparing(trade -> trade.getTimestamp().getSeconds()))
+        .distinct()
+        .collect(toImmutableList());
+  }
+
   private static Instant getStartTime(GranularitySpec granularitySpec, ExchangeTrade firstTrade) {
     long firstTradeSeconds = firstTrade.getTimestamp().getSeconds();
     long secondsAfterStart = firstTradeSeconds % granularitySpec.seconds();
@@ -63,7 +100,7 @@ public class CandleFactory {
 
     abstract Granularity granularity();
 
-    abstract ImmutableList<ExchangeTrade> trades();
+    abstract ImmutableSet<ExchangeTrade> trades();
 
     @AutoValue.Builder
     public abstract static class Builder {
@@ -73,5 +110,41 @@ public class CandleFactory {
 
       public abstract CreateParams build();
     }
+  }
+
+  @AutoValue
+  public abstract static class MergeParams {
+    public static MergeParams create(
+        ImmutableSet<Candle> candles, Granularity fromGranularity, Granularity toGranularity) {
+      GranularitySpec fromSpec = GranularitySpec.fromGranularity(fromGranularity);
+      GranularitySpec toSpec = GranularitySpec.fromGranularity(toGranularity);
+      checkArgument(toSpec.minutes() % fromSpec.minutes() == 0);
+      checkArgument(candles.size() == toSpec.minutes() / (fromSpec.minutes()));
+
+      ImmutableList<Candle> sortedCandles = getSortedCandles(candles, fromSpec);
+      return new AutoValue_CandleFactory_MergeParams(ImmutableSet.copyOf(sortedCandles));
+    }
+
+    private static ImmutableList<Candle> getSortedCandles(
+        ImmutableSet<Candle> candles, GranularitySpec fromSpec) {
+      ImmutableList<Candle> sortedCandles =
+          candles.stream()
+              .sorted(comparing(candle -> candle.getStart().getSeconds()))
+              .collect(toImmutableList());
+
+      ImmutableSet<Long> expectedStartTimes =
+          Stream.iterate(
+                  sortedCandles.get(0).getStart().getSeconds(), start -> start + fromSpec.seconds())
+              .limit(sortedCandles.size())
+              .collect(toImmutableSet());
+      ImmutableSet<Long> actualStartTimes =
+          sortedCandles.stream()
+              .map(candle -> candle.getStart().getSeconds())
+              .collect(toImmutableSet());
+      checkArgument(expectedStartTimes.equals(actualStartTimes));
+      return sortedCandles;
+    }
+
+    abstract ImmutableSet<Candle> candles();
   }
 }
