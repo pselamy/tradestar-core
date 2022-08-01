002aa214004095f97bb5505abdcb1da76fcc3351
Patrick Selamy <pselamy@gmail.com>
2022-07-31 23:08:58 -0500

    Remove old artifacts.

--- src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
@@ -6,17 +6,13 @@ import com.verlumen.tradestar.core.candles.GranularitySpec;
 import com.verlumen.tradestar.core.shared.CandleSorter;
 import com.verlumen.tradestar.protos.candles.Candle;
 import com.verlumen.tradestar.protos.candles.Granularity;
-import org.apache.commons.lang3.tuple.Pair;
 import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
 
 import java.time.Duration;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
-import static java.time.Instant.ofEpochSecond;
 
 public class BarSeriesFactory {
   public static BarSeries create(Granularity granularity, ImmutableSet<Candle> candles) {
@@ -31,10 +27,5 @@ public class BarSeriesFactory {
             .map(candle -> BarFactory.create(duration, candle))
             .collect(toImmutableList());
     return new BaseBarSeriesBuilder().withBars(bars).build();
-  }
-
-  private static <T> Stream<Pair<T, T>> adjacentCandles(ImmutableList<T> candles) {
-    return IntStream.rangeClosed(0, candles.size() - 2)
-        .mapToObj(i -> Pair.of(candles.get(i), candles.get(i + 1)));
   }
 }
