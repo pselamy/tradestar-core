a7eb4d47585fab3d0d1ab748c6f090e7cd3cee36
Patrick Selamy <pselamy@gmail.com>
2022-07-31 21:41:49 -0500

    Add RuleFactoryImpl

--- src/main/java/com/verlumen/tradestar/core/ta/rules/RuleFactory.java
+++ src/main/java/com/verlumen/tradestar/core/ta/rules/RuleFactory.java
@@ -1,13 +1,25 @@
 package com.verlumen.tradestar.core.ta.rules;
 
+import com.google.common.collect.ImmutableSet;
 import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import com.verlumen.tradestar.protos.strategies.TradeStrategy.StrategyOneOfCase;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Rule;
 
 public interface RuleFactory {
+  Rule buyRule(TradeStrategy params, BarSeries barSeries);
+
+  Rule sellRule(TradeStrategy params, BarSeries barSeries);
+
+  default StrategyOneOfCase supportedCase() {
+    return StrategyOneOfCase.STRATEGYONEOF_NOT_SET;
+  }
+
+  interface Adapter {
     Rule buyRule(TradeStrategy params, BarSeries barSeries);
 
     Rule sellRule(TradeStrategy params, BarSeries barSeries);
 
-    TradeStrategy.StrategyOneOfCase supportedCase();
+    StrategyOneOfCase supportedCase();
+  }
 }
