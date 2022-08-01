477c644852b4ea67040a25b42e9dc0cb4ddd08aa
Patrick Selamy <pselamy@gmail.com>
2022-08-01 01:13:46 -0400

    Update BarSeriesManagerFactory.java

--- src/main/java/com/verlumen/tradestar/core/backtesting/BarSeriesManagerFactory.java
+++ src/main/java/com/verlumen/tradestar/core/backtesting/BarSeriesManagerFactory.java
@@ -1,9 +1,27 @@
 package com.verlumen.tradestar.core.backtesting;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.verlumen.tradestar.protos.candles.Candle;
+import com.verlumen.tradestar.protos.candles.Granularity;
 import org.ta4j.core.BarSeriesManager;
 
 interface BarSeriesManagerFactory {
-    BarSeriesManager create(ImmutableList<Candle> candles);
+    BarSeriesManager create(CreateParams params);
+    
+    @AutoValue
+    abstract static class CreateParams {
+        static Builder builder() {
+            return new AutoValue_BarSeriesManager_CreateParams.Builder();   
+        }
+        
+        ImmutableSet<Candle> candles();
+        Granularity granularity();
+
+        @AutoValue.Builder
+        abstract static class Builder() {
+            abstract Builder setCandles(ImmutableSet<Candle> candles);
+            abstract Builder setGranularity(Granularity granularity);
+            abstract CreateParams build();
+        }
+    }
 }
