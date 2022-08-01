f586871c1d29611a6ac149c885861ed8ed2e202d
Patrick Selamy <pselamy@gmail.com>
2022-08-01 00:53:16 -0400

    Create BarSeriesManagerFactory.java

---
+++ src/main/java/com/verlumen/tradestar/core/backtesting/BarSeriesManagerFactory.java
@@ -0,0 +1,9 @@
+package com.verlumen.tradestar.backtesting;
+
+import com.google.common.collect.ImmutableList;
+import com.verlumen.tradestar.protos.candles.Candle;
+import org.ta4j.core.BarSeriesManager;
+
+interface BarSeriesManagerFactory {
+    BarSeriesManager create(ImmutableList<Candle> candles);
+}
