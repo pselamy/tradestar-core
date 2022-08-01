08b0597bf861d2dbd62486321faf00386c2e6938
Patrick Selamy <pselamy@gmail.com>
2022-07-31 23:06:55 -0500

    Add StrategiesModule

---
+++ src/main/java/com/verlumen/tradestar/core/ta/strategies/StrategyFactory.java
@@ -0,0 +1,9 @@
+package com.verlumen.tradestar.core.ta.strategies;
+
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Strategy;
+
+public interface StrategyFactory {
+    Strategy create(TradeStrategy params, BarSeries barSeries);
+}
