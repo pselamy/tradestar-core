45c98abb2f41384c8e20e6af5b1ddcfeabe45c33
Patrick Selamy <pselamy@gmail.com>
2022-07-31 20:50:25 -0500

    Add `rules` and `signalstrength` packages.

---
+++ src/main/java/com/verlumen/tradestar/core/ta/rules/RuleFactory.java
@@ -0,0 +1,13 @@
+package com.verlumen.tradestar.core.ta.rules;
+
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Rule;
+
+public interface RuleFactory {
+    Rule buyRule(TradeStrategy params, BarSeries barSeries);
+
+    Rule sellRule(TradeStrategy params, BarSeries barSeries);
+
+    TradeStrategy.StrategyOneOfCase supportedCase();
+}
