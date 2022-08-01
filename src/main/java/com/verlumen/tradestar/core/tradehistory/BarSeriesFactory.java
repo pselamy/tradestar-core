09408ca06a2f89adf6a2b440a0fa75db52773f08
Patrick Selamy <pselamy@gmail.com>
2022-07-31 20:03:30 -0500

    Add `CandleFactory`.

--- src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
@@ -1,47 +1,36 @@
 package com.verlumen.tradestar.core.tradehistory;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static java.time.Instant.ofEpochSecond;
-
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.verlumen.tradestar.core.candles.GranularitySpec;
+import com.verlumen.tradestar.core.shared.CandleSorter;
 import com.verlumen.tradestar.protos.candles.Candle;
 import com.verlumen.tradestar.protos.candles.Granularity;
-import java.time.Duration;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import org.apache.commons.lang3.tuple.Pair;
 import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
 
+import java.time.Duration;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.time.Instant.ofEpochSecond;
+
 public class BarSeriesFactory {
-  public static BarSeries create(Granularity granularity, ImmutableList<Candle> candles) {
+  public static BarSeries create(Granularity granularity, ImmutableSet<Candle> candles) {
     GranularitySpec granularitySpec = GranularitySpec.create(granularity);
-    checkArgument(candlesAreConforming(granularitySpec, candles));
     Duration duration = granularitySpec.duration();
-    return create(duration, candles);
+    return create(duration, CandleSorter.sortAndValidate(candles, granularitySpec));
   }
 
-  private static BarSeries create(Duration duration, ImmutableList<Candle> candles) {
+  private static BarSeries create(Duration duration, ImmutableSet<Candle> candles) {
     ImmutableList<Bar> bars =
         candles.stream()
             .map(candle -> BarFactory.create(duration, candle))
             .collect(toImmutableList());
     return new BaseBarSeriesBuilder().withBars(bars).build();
-  }
-
-  public static boolean candlesAreConforming(
-      GranularitySpec granularitySpec, ImmutableList<Candle> candles) {
-    return candles.size() <= 1
-        || adjacentCandles(candles)
-            .map(
-                pair ->
-                    Duration.between(
-                        ofEpochSecond(pair.getLeft().getStart().getSeconds()),
-                        ofEpochSecond(pair.getRight().getStart().getSeconds())))
-            .allMatch(duration -> duration.equals(granularitySpec.duration()));
   }
 
   private static <T> Stream<Pair<T, T>> adjacentCandles(ImmutableList<T> candles) {
