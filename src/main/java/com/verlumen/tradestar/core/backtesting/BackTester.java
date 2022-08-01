8e330a752a12eda60aac6c522f4b96a2121911b1
Patrick Selamy <pselamy@gmail.com>
2022-08-01 01:06:29 -0400

    Update BackTester.java

--- src/main/java/com/verlumen/tradestar/core/backtesting/BackTester.java
+++ src/main/java/com/verlumen/tradestar/core/backtesting/BackTester.java
@@ -1,8 +1,13 @@
 package com.verlumen.tradestar.core.backtesting;
 
 import com.verlumen.tradestar.protos.backtesting.BackTestRequest;
-import com.verlumen.tradestar.protos.backtesting.BackTestResult;
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import com.verlumen.tradestar.protos.strategies.TradeStrategyTestResult;
 
 public interface BackTester {
-    BackTestResult run(BackTestRequest request);
+    TradeStrategyTestResult test(TestParams request);
+    
+    interface TestParams {
+        TradeStrategy tradeStrategy();
+    }
 }
