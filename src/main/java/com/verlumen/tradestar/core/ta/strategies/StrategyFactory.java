19827d88fa285e705ef4b0378b85e0fa3df8c297
Patrick Selamy <pselamy@gmail.com>
2022-10-27 00:24:46 -0400

    Remove legacy strategies. (#25)

--- src/main/java/com/verlumen/tradestar/core/ta/strategies/StrategyFactory.java
+++
@@ -1,9 +0,0 @@
-package com.verlumen.tradestar.core.ta.strategies;
-
-import com.verlumen.tradestar.protos.strategies.TradeStrategy;
-import org.ta4j.core.BarSeries;
-import org.ta4j.core.Strategy;
-
-public interface StrategyFactory {
-  Strategy create(TradeStrategy params, BarSeries barSeries);
-}
