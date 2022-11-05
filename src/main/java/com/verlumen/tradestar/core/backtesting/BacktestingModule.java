2560f7f78e9d93335a6c6a6bc5880c0899fb5265
Patrick Selamy <pselamy@gmail.com>
2022-11-05 16:42:32 -0400

    Install AdaptersModule (#29)

--- src/main/java/com/verlumen/tradestar/core/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestar/core/backtesting/BacktestingModule.java
@@ -1,10 +1,13 @@
 package com.verlumen.tradestar.core.backtesting;
 
 import com.google.inject.AbstractModule;
+import com.verlumen.tradestar.core.strategies.adapters.AdaptersModule;
 
 public class BacktestingModule extends AbstractModule {
   @Override
   protected void configure() {
+    install(new AdaptersModule());
+
     bind(Backtester.class).to(BacktesterImpl.class);
   }
 }
