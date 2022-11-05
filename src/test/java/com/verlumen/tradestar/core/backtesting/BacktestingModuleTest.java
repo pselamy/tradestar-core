0a001f4ab722f8c4cc073970f9be44ee2e6fb87a
Patrick Selamy <pselamy@gmail.com>
2022-11-05 16:58:36 -0400

    Add preliminary test

---
+++ src/test/java/com/verlumen/tradestar/core/backtesting/BacktestingModuleTest.java
@@ -0,0 +1,22 @@
+package com.verlumen.tradestar.core.backtesting;
+
+import com.google.inject.Guice;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BacktestingModuleTest {
+  private BacktestingModule underTest;
+
+  @Before
+  public void setUp() {
+    underTest = new BacktestingModule();
+  }
+
+  @Test
+  public void createInjector_consumesModule() {
+    Guice.createInjector(underTest);
+  }
+}
