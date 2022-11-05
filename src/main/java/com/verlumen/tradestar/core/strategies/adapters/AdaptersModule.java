1b3186abe2965b188a58adbdcf9ade2e7980b2fa
Patrick Selamy <pselamy@gmail.com>
2022-11-05 17:02:57 -0400

    Add preliminary test

--- src/main/java/com/verlumen/tradestar/core/strategies/adapters/AdaptersModule.java
+++ src/main/java/com/verlumen/tradestar/core/strategies/adapters/AdaptersModule.java
@@ -1,19 +1,18 @@
 package com.verlumen.tradestar.core.strategies.adapters;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 
 public class AdaptersModule extends AbstractModule {
-  private static final ImmutableSet<Class<? extends TradeStrategyAdapter>> TRADE_STRATEGY_ADAPTERS =
-      ImmutableSet.of(AdxTradeStrategyAdapter.class, CompositeTradeStrategyAdapter.class);
-
   @Override
   protected void configure() {
-    Multibinder<TradeStrategyAdapter> tradeStrategyAdapterMultibinder =
-        newSetBinder(binder(), TradeStrategyAdapter.class);
-    TRADE_STRATEGY_ADAPTERS.forEach(tradeStrategyAdapterMultibinder.addBinding()::to);
+    bindTradeStrategyAdapters(newSetBinder(binder(), TradeStrategyAdapter.class));
+  }
+
+  private void bindTradeStrategyAdapters(Multibinder<TradeStrategyAdapter> multibinder) {
+    multibinder.addBinding().to(AdxTradeStrategyAdapter.class);
+    multibinder.addBinding().to(CompositeTradeStrategyAdapter.class);
   }
 }
