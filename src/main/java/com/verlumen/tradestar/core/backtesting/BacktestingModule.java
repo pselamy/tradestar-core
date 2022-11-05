98246a76752e0b1e84df773de10ef2fa0611bf60
Patrick Selamy <pselamy@gmail.com>
2022-11-05 19:51:31 -0400

    Update BacktestingModule

--- src/main/java/com/verlumen/tradestar/core/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestar/core/backtesting/BacktestingModule.java
@@ -1,13 +1,21 @@
 package com.verlumen.tradestar.core.backtesting;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.verlumen.tradestar.core.strategies.adapters.AdaptersModule;
+import com.verlumen.tradestar.core.strategies.adapters.TradeStrategyAdapter;
+
+import java.util.Set;
 
 public class BacktestingModule extends AbstractModule {
   @Override
   protected void configure() {
     install(new AdaptersModule());
+  }
 
-    bind(Backtester.class).to(BacktesterImpl.class);
+  @Provides
+  Backtester backtester(Set<TradeStrategyAdapter> adapters, TestResultFactory testResultFactory) {
+    return Backtester.create(ImmutableSet.copyOf(adapters), testResultFactory);
   }
 }
