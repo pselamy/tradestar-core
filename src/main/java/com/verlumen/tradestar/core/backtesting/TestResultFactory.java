7688208f86064f4bb594b010053abd0ad6c7c2a1
Patrick Selamy <pselamy@gmail.com>
2022-11-06 00:39:47 -0400

    Only write values that are non-zero numbers. (#35)

--- src/main/java/com/verlumen/tradestar/core/backtesting/TestResultFactory.java
+++ src/main/java/com/verlumen/tradestar/core/backtesting/TestResultFactory.java
@@ -12,6 +12,7 @@ import org.ta4j.core.BarSeries;
 import org.ta4j.core.TradingRecord;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 import static java.lang.Math.max;
 
@@ -30,62 +31,65 @@ class TestResultFactory implements Serializable {
 
     abstract TradingRecord record();
 
-    private double doubleValue(Criterion criterion) {
+    private Optional<Double> doubleValue(Criterion criterion) {
       return criterion.doubleValue(series(), record());
     }
 
-    private int intValue(Criterion criterion) {
+    private Optional<Integer> intValue(Criterion criterion) {
       return criterion.intValue(series(), record());
     }
 
     @Memoized
     MaxDrawdownReport maxDrawdownReport() {
-      return MaxDrawdownReport.newBuilder()
-          .setAmount(doubleValue(Criterion.MAX_DRAWDOWN))
-          .setReturn(doubleValue(Criterion.RETURN_OVER_MAX_DRAWDOWN))
-          .build();
+      MaxDrawdownReport.Builder builder = MaxDrawdownReport.newBuilder();
+      doubleValue(Criterion.MAX_DRAWDOWN).ifPresent(builder::setAmount);
+      doubleValue(Criterion.RETURN_OVER_MAX_DRAWDOWN).ifPresent(builder::setReturn);
+      return builder.build();
     }
 
     @Memoized
     PositionReport positionReport() {
-      int count = intValue(Criterion.NUM_POS);
-      int breakEven = intValue(Criterion.NUM_BREAK_EVEN_POS);
-      int losing = intValue(Criterion.NUM_LOSING_POS);
-      int winning = max(count - breakEven - losing, 0);
-      return PositionReport.newBuilder()
-          .setBreakEven(breakEven)
-          .setConsecutiveWinning(intValue(Criterion.NUM_CONSEC_WIN_POS))
-          .setCount(count)
-          .setLosing(losing)
-          .setLosingRatio(doubleValue(Criterion.LOSING_POS_RATIO))
-          .setWinning(winning)
-          .setWinningRatio(doubleValue(Criterion.WINNING_POS_RATIO))
+      return intValue(Criterion.NUM_POS)
+          .filter(positionCount -> positionCount > 0)
+          .map(PositionReport.newBuilder()::setCount)
+          .flatMap(
+              builder -> intValue(Criterion.NUM_CONSEC_WIN_POS).map(builder::setConsecutiveWinning))
+          .flatMap(builder -> intValue(Criterion.NUM_BREAK_EVEN_POS).map(builder::setBreakEven))
+          .flatMap(builder -> intValue(Criterion.NUM_LOSING_POS).map(builder::setLosing))
+          .flatMap(builder -> intValue(Criterion.LOSING_POS_RATIO).map(builder::setLosingRatio))
+          .flatMap(builder -> intValue(Criterion.WINNING_POS_RATIO).map(builder::setWinningRatio))
+          .map(
+              builder ->
+                  builder.setWinning(
+                      max(builder.getCount() - builder.getBreakEven() - builder.getLosing(), 0)))
+          .orElseGet(PositionReport::newBuilder)
           .build();
     }
 
     @Memoized
     ProfitLossReport profitLossReport() {
-      return ProfitLossReport.newBuilder()
-          .setAmount(doubleValue(Criterion.PROFIT_LOSS))
-          .setAverageLoss(doubleValue(Criterion.AVG_LOSS))
-          .setAverageProfit(doubleValue(Criterion.AVG_PROFIT))
-          .setGrossLoss(doubleValue(Criterion.GROSS_LOSS))
-          .setGrossProfit(doubleValue(Criterion.GROSS_PROFIT))
-          .setNetLoss(doubleValue(Criterion.NET_LOSS))
-          .setNetProfit(doubleValue(Criterion.NET_PROFIT))
-          .setPercentage(doubleValue(Criterion.PROFIT_LOSS_PERCENTAGE))
-          .setRatio(doubleValue(Criterion.PROFIT_LOSS_RATIO))
-          .build();
+      ProfitLossReport.Builder builder = ProfitLossReport.newBuilder();
+      doubleValue(Criterion.PROFIT_LOSS).ifPresent(builder::setAmount);
+      doubleValue(Criterion.AVG_LOSS).ifPresent(builder::setAverageLoss);
+      doubleValue(Criterion.AVG_PROFIT).ifPresent(builder::setAverageProfit);
+      doubleValue(Criterion.GROSS_LOSS).ifPresent(builder::setGrossLoss);
+      doubleValue(Criterion.GROSS_PROFIT).ifPresent(builder::setGrossProfit);
+      doubleValue(Criterion.NET_LOSS).ifPresent(builder::setNetLoss);
+      doubleValue(Criterion.NET_PROFIT).ifPresent(builder::setNetProfit);
+      doubleValue(Criterion.NET_PROFIT).ifPresent(builder::setNetProfit);
+      doubleValue(Criterion.PROFIT_LOSS_PERCENTAGE).ifPresent(builder::setPercentage);
+      doubleValue(Criterion.PROFIT_LOSS_RATIO).ifPresent(builder::setRatio);
+      return builder.build();
     }
 
     @Memoized
     ReturnReport returnReport() {
-      return ReturnReport.newBuilder()
-          .setAveragePerBar(doubleValue(Criterion.AVG_RETURN_PER_BAR))
-          .setBuyAndHold(doubleValue(Criterion.BUY_AND_HOLD_RETURN))
-          .setExpectancy(doubleValue(Criterion.EXPECTANCY))
-          .setGross(doubleValue(Criterion.GROSS_RETURN))
-          .build();
+      ReturnReport.Builder builder = ReturnReport.newBuilder();
+      doubleValue(Criterion.AVG_RETURN_PER_BAR).ifPresent(builder::setAveragePerBar);
+      doubleValue(Criterion.BUY_AND_HOLD_RETURN).ifPresent(builder::setBuyAndHold);
+      doubleValue(Criterion.EXPECTANCY).ifPresent(builder::setExpectancy);
+      doubleValue(Criterion.GROSS_RETURN).ifPresent(builder::setGross);
+      return builder.build();
     }
 
     @Memoized
